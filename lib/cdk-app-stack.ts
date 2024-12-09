9856c8c9a092ddeb37b8dd1bc3f589b60be45abc
Swaroop Kori <swaroop.kori@intimetec.com>
2024-12-09 22:37:51 +0530

    cdk learning

--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3object from 'aws-cdk-lib/aws-s3-deployment'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 
 export class CdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new s3.Bucket(this, 'mybucketuniqueid', {
+      versioned: true
+    });
+    new s3object.BucketDeployment(this, 'deployfile' ,{
+      sources: [s3object.Source.asset('./s3')],
+      destinationBucket: bucket
+    })
+    const lambdaFn=new lambda.Function(this,'Mylambdafunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code:lambda.Code.fromAsset('lambda')
+    
+    })
   }
 }
